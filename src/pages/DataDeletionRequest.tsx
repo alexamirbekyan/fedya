86ccec5d4433e5407ef514c4e646b47b94e373fb
alex <alex.amirbekyan@gmail.com>
2022-09-02 18:06:35 +0400

    Added memopin embed.

--- src/pages/DataDeletionRequest.tsx
+++ src/pages/DataDeletionRequest.tsx
@@ -3,8 +3,21 @@ import React from 'react';
 function DataDeletionRequest() {
   return (
     <div>
-      DDR
+
+      <iframe
+        title="Memopin Embed Map"
+        src="https://memopin-io-development.web.app/maps/embed?list=TNJLwVLbTkeNpyI6md1gIKjhrow1|FbEEBCrrmznASMkPtJMF"
+        width="100%"
+        height="600"
+        // style="border: 0;border-radius: 8px; margin: 4px;"
+        // allowFullScreen=""
+        scrolling="no"
+        loading="lazy"
+      ></iframe>
+
     </div>
+
+
   );
 }
 
