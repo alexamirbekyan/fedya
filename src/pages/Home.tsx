683d0eac766df80253fc36999de88961cb88d9d8
alex <alex.amirbekyan@gmail.com>
2022-09-02 11:17:44 +0400

    Added memopin embed.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,17 @@ function Home() {
     <div>
       <h1>Welcome to Fedya</h1>
 
+
+      <iframe
+        src="https://memopin-io-development.web.app/maps/embed?list=TNJLwVLbTkeNpyI6md1gIKjhrow1|FbEEBCrrmznASMkPtJMF"
+        width="100%"
+        height="600"
+        style="border:0; border-radius: 8px; margin: 4px;"
+        allowFullScreen=""
+        scrolling="no"
+        loading="lazy"></iframe>
+
+
       <div>
         <Link to={'/privacy'}>Privacy</Link>
         <Link to={'/ddi'}>DDI</Link>
